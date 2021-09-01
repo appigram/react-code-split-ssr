d3d55640f4d01734de2aa578ccd3b5c906ca2756
Evgeny Sysmanov <appigram@gmail.com>
2021-09-01 10:41:48 +0300

    Back to  React-router 5, bump deps

--- src/generate-routes.tsx
+++ src/generate-routes.tsx
@@ -1,73 +1,80 @@
-import {FunctionComponentElement} from 'react'
-import { matchPath, Navigate, Route, Routes } from 'react-router-dom'
-// import Bundle from './bundle'
+import { FunctionComponentElement } from "react";
+import { matchPath, Redirect, Route, Switch } from "react-router-dom";
+import Bundle from "./bundle";
 
 export interface IJSXModule {
-  default: React.FC | React.ComponentClass
+	default: React.FC<{}> | null;
 }
 
 export interface ISSRRoute {
-  path: string
-  element: () => FunctionComponentElement<{ mod: Promise<IJSXModule> }>
-  exact?: boolean
-  strict?: boolean
+	path: string;
+	component: () => FunctionComponentElement<{ mod: Promise<IJSXModule> }>;
+	caseSensitive?: boolean;
+	children?: React.ReactNode;
+	exact?: boolean;
+	strict?: boolean;
 }
 
 export interface IRedirects {
-  from: string
-  to: string | object
-  push?: boolean
+	from: string;
+	to: string | object;
+	push?: boolean;
 }
 
 export interface IOptions {
-  pathname: string
-  routes: ISSRRoute[],
-  redirects?: IRedirects[],
-  notFoundComp?: () => FunctionComponentElement<{ mod: Promise<IJSXModule> }>
+	pathname: string;
+	routes: ISSRRoute[];
+	redirects?: IRedirects[];
+	notFoundComp?: () => FunctionComponentElement<{ mod: Promise<IJSXModule> }>;
 }
 
 const generateRoutes = async (
-  options: IOptions = {
-    pathname: '/',
-    routes: [],
-    redirects: [],
-  },
+	options: IOptions = {
+		pathname: "/",
+		routes: [],
+		redirects: [],
+	}
 ): Promise<React.FC> => {
-  if (!Array.isArray(options.routes) || options.routes.length === 0) {
-    throw new Error('options.routes must be an non-empty array')
-  }
+	if (!Array.isArray(options.routes) || options.routes.length === 0) {
+		throw new Error("options.routes must be an non-empty array");
+	}
 
-  if (!Array.isArray(options.redirects)) {
-    throw new Error('options.redirects must be an array')
-  }
+	if (!Array.isArray(options.redirects)) {
+		throw new Error("options.redirects must be an array");
+	}
 
-  const preload = options.routes.find((route) =>
-    !!matchPath(options.pathname, route.path)
-  )
+	const preload = options.routes.find(
+		(route) => !!matchPath(options.pathname, route.path)
+	);
 
-  const preloadedComp: IJSXModule = preload === undefined
-    ? await options.notFoundComp().props.mod
-    : await preload.element().props.mod
+	const preloadedComp: IJSXModule =
+		preload === undefined
+			? await options.notFoundComp().props.mod
+			: await preload.component().props.mod;
 
-  const renderComp = (path: string, bundle: React.FC) => {
-    if (!preloadedComp) return bundle
-    const isSSR = (preload && preload.path === path) || (!preload && !path)
-    return isSSR ? preloadedComp.default : bundle
-  }
+	const renderComp = (path: string, bundle: React.FC) => {
+		if (!preloadedComp) return bundle;
+		const isSSR = (preload && preload.path === path) || (!preload && !path);
+		return isSSR ? preloadedComp.default : bundle;
+	};
 
-  return () => {
-    return (
-      <Routes>
-        {options.routes.map((props, i) => (
-          <Route key={i} {...props} element={renderComp(props.path, props.element)} />
-        ))}
-        {options.redirects.map((props, i) => (
-          <Navigate key={i} {...props} />
-        ))}
-        <Route element={renderComp(null, options.notFoundComp)} />
-      </Routes>
-    )
-  }
-}
+	return () => {
+		return (
+			<Switch>
+				{options.routes.map((props, i) => (
+					<Route
+						key={i}
+						{...props}
+						component={renderComp(props.path, props.component)}
+					/>
+				))}
+				{options.redirects.map((props, i) => (
+					<Redirect key={i} {...props} />
+				))}
+				<Route component={renderComp(null, options.notFoundComp)} />
+			</Switch>
+		);
+	};
+};
 
-export default generateRoutes
+export default generateRoutes;
